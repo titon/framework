508a459bdcdf8c61147f38296110d68f5a6cc442
Miles Johnson <mileswjohnson@gmail.com>
2014-06-28 12:50:03 -0700

    More work on vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -2,6 +2,8 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "chef/ubuntu-13.10"
-  config.vm.provision :shell, path: "bin/provision-vagrant.sh"
+  config.vm.box = "ubuntu/trusty64"
+
+  # Uncomment this once the provision script has been fully tested
+  # config.vm.provision :shell, path: "bin/provision-vagrant.sh"
 end
--- bin/provision-vagrant.sh
+++ bin/provision-vagrant.sh
@@ -5,15 +5,14 @@ sudo apt-get update
 sudo apt-get -y upgrade
 
 # Install packages
-sudo apt-get -y install git
-sudo apt-get -y install curl
-sudo apt-get -y install vim
+sudo apt-get -y install git curl vim
 
 # Install HHVM
 wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
-echo deb http://dl.hhvm.com/ubuntu saucy main | sudo tee /etc/apt/sources.list.d/hhvm.list
+echo deb http://dl.hhvm.com/ubuntu trusty main | sudo tee /etc/apt/sources.list.d/hhvm.list
 sudo apt-get update
-sudo apt-get -y install hhvm
+sudo apt-get -y install hhvm-nightly
+sudo /usr/bin/update-alternatives --install /usr/bin/php php /usr/bin/hhvm 60
 
 # Install Composer
 wget -O /vagrant/composer.phar https://getcomposer.org/composer.phar

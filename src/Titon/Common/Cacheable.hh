323c9e709df0fcfe86e616b572c34e062f8d3b69
Miles Johnson <mileswjohnson@gmail.com>
2014-10-22 22:32:09 -0700

    Type checker fixes.

--- src/Titon/Common/Cacheable.hh
+++ src/Titon/Common/Cacheable.hh
@@ -43,10 +43,10 @@ trait Cacheable {
      * If the value happens to be a closure, evaluate the closure and save the result.
      *
      * @param mixed $key
-     * @param (function(): mixed) $callback
+     * @param (function(mixed): mixed) $callback
      * @return mixed
      */
-    public function cache(mixed $key, (function(): mixed) $callback): mixed {
+    public function cache(mixed $key, (function(mixed): mixed) $callback): mixed {
         $key = $this->createCacheKey($key);
 
         if ($cache = $this->getCache($key)) {
--- src/Titon/Debug/Debugger.hh
+++ src/Titon/Debug/Debugger.hh
@@ -393,7 +393,7 @@ class Debugger {
             $error = static::getError((int) $code);
         }
 
-        switch ($code) {
+        switch ((int) $code) {
             case E_ERROR:
             case E_PARSE:
             case E_CORE_ERROR:

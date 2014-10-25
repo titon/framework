2a94e3ba6b3ff13b654cd6b3a439ff36298df0f4
Miles Johnson <mileswjohnson@gmail.com>
2014-10-24 19:44:07 -0700

    Test fixes.

--- src/Titon/Route/Router.hh
+++ src/Titon/Route/Router.hh
@@ -147,8 +147,8 @@ class Router {
         });
 
         // Set caching events
-        $this->on('route.matching', inst_meth($this, 'loadRoutes'), Map {'priority' => 1});
-        $this->on('route.matched', inst_meth($this, 'cacheRoutes'), Map {'priority' => 1});
+        $this->on('route.matching', inst_meth($this, 'loadRoutes'), 1);
+        $this->on('route.matched', inst_meth($this, 'cacheRoutes'), 1);
     }
 
     /**

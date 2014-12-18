6bd9cb2f51e55da51eba670413647ebd593c0e3e
Miles Johnson <miles.johnson@hyfn.com>
2014-12-17 16:01:31 -0800

    Reworked route grouping by adding a new Group class. Provided mixins for shared functionality.

---
+++ src/Titon/Route/Mixin/FilterMixin.hh
@@ -0,0 +1,75 @@
+<?hh // strict
+/**
+ * @copyright   2010-2013, The Titon Project
+ * @license     http://opensource.org/licenses/bsd-license.php
+ * @link        http://titon.io
+ */
+
+namespace Titon\Route\Mixin;
+
+type FilterList = Vector<string>;
+
+/**
+ * Provides functionality for filters.
+ *
+ * @package Titon\Route\Mixin
+ */
+trait FilterMixin {
+
+    /**
+     * Filters to trigger once the route has been matched.
+     *
+     * @type \Titon\Route\Mixin\FilterList
+     */
+    protected FilterList $_filters = Vector {};
+
+    /**
+     * Add a filter by name.
+     *
+     * @param string $filter
+     * @return $this
+     */
+    public function addFilter(string $filter): this {
+        if (!in_array($filter, $this->_filters)) {
+            $this->_filters[] = $filter;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Add multiple filters by name.
+     *
+     * @param \Titon\Route\Mixin\FilterList $filters
+     * @return $this
+     */
+    public function addFilters(FilterList $filters): this {
+        foreach ($filters as $filter) {
+            $this->addFilter($filter);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Return all filters.
+     *
+     * @return \Titon\Route\Mixin\FilterList
+     */
+    public function getFilters(): FilterList {
+        return $this->_filters;
+    }
+
+    /**
+     * Set the list of filters to process.
+     *
+     * @param \Titon\Route\Mixin\FilterList $filters
+     * @return $this
+     */
+    public function setFilters(FilterList $filters): this {
+        $this->_filters = $filters;
+
+        return $this;
+    }
+
+}

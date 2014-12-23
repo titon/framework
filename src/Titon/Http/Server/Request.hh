319cee853bed8f8260477788df44459924eb6c1a
Miles Johnson <mileswjohnson@gmail.com>
2014-12-22 18:36:05 -0800

    Update copyright to 2015.

--- src/Titon/Http/Server/Request.hh
+++ src/Titon/Http/Server/Request.hh
@@ -1,7 +1,7 @@
 <?hh // partial
 // Because of PSR HTTP Message
 /**
- * @copyright   2010-2014, The Titon Project
+ * @copyright   2010-2015, The Titon Project
  * @license     http://opensource.org/licenses/bsd-license.php
  * @link        http://titon.io
  */

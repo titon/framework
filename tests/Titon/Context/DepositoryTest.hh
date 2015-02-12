60606b3f18d0fb4389555a5ab0cea923ea4c122f
Alex Phillips <exonintrendo@gmail.com>
2015-02-12 17:30:28 -0500

    more tests

--- tests/Titon/Context/DepositoryTest.hh
+++ tests/Titon/Context/DepositoryTest.hh
@@ -21,24 +21,114 @@ class DepositoryTest extends TestCase
         $this->object = new Depository();
     }
 
+    public function testRegisterClassName()
+    {
+        $this->object->register('foo', 'Titon\\Context\\Foo');
+        $this->assertInstanceOf('Titon\\Context\\Foo', $this->object['foo']);
+    }
+
     public function testClosure()
     {
-        $this->object->add('test', function() {
-            $test = new \StdClass();
-            $test->foo = 'bar';
+        $this->object->register('bar', function() {
+            $test = new Bar((new Foo())->setName('Foo Bar'));
 
             return $test;
         });
 
-        $this->assertEquals('bar', $this->object->make('test')->foo);
+        $this->assertInstanceOf('Titon\\Context\\Bar', $this->object['bar']);
+        $this->assertEquals('Foo Bar', $this->object['bar']->getFoo()->getName());
     }
 
     public function testSingleton()
     {
-        $test = new \StdClass();
-        $test->foo = 'bar';
-        $this->object->singleton('test', $test);
+        $test = new Foo();
+        $this->object->singleton('foo', $test);
 
-        $this->assertEquals('bar', $this->object->make('test')->foo);
+        $this->assertEquals('Alex Phillips', $this->object['foo']->getName());
+
+        $test = new Bar((new Foo())->setName('Foo Bar'));
+        $this->object->singleton('bar', $test);
+
+        $test = $this->object['bar'];
+        $this->assertInstanceOf('Titon\\Context\\Bar', $test);
+        $this->assertEquals('Foo Bar', $test->getFoo()->getName());
+    }
+
+    public function testAutoDependencyResolution()
+    {
+        $test = $this->object['Titon\\Context\\Bar'];
+        $this->assertInstanceOf('Titon\\Context\\Bar', $test);
+        $this->assertInstanceOf('Titon\\Context\\Foo', $test->getFoo());
+    }
+
+    public function testClassDefinitionWithMethodCalls()
+    {
+        $this->object->register('foo', 'Titon\\Context\\Foo')->call('setName', 'Foo Bar');
+        $this->assertInstanceOf('Titon\\Context\\Foo', $this->object['foo']);
+        $this->assertEquals('Foo Bar', $this->object['foo']->getName());
+    }
+
+    public function testCallMethodsWithObjects()
+    {
+        $test = $this->object['Titon\\Context\\Bar']->getFoo();
+        $this->assertInstanceOf('Titon\\Context\\Foo', $test);
+    }
+
+    public function testConstructorInjection()
+    {
+        $this->object->register('bar', 'Titon\\Context\\Bar')->with('Titon\\Context\\Foo');
+        $this->assertInstanceOf('Titon\\Context\\Bar', $this->object['bar']);
+
+        $this->object->register('bar', 'Titon\\Context\\Bar')->with((new Foo())->setName('Foo Bar'));
+        $this->assertInstanceOf('Titon\\Context\\Bar', $this->object['bar']);
+        $this->assertEquals($this->object['bar']->getFoo()->getName(), 'Foo Bar');
+    }
+
+    public function testMethodInjection()
+    {
+        $this->object->register('foo', 'Titon\\Context\\Foo')->call('setName', 'Foo Bar');
+        $this->assertEquals('Foo Bar', $this->object['foo']->getName());
+    }
+}
+
+class Foo
+{
+    public $name;
+
+    public function __construct($name = 'Alex Phillips')
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
+
+class Bar
+{
+    private $foo;
+
+    public function __construct(Foo $foo)
+    {
+        $this->foo = $foo;
+    }
+
+    public function getFoo()
+    {
+        return $this->foo;
+    }
+
+    public function setFoo($foo)
+    {
+        $this->foo = $foo;
     }
 }

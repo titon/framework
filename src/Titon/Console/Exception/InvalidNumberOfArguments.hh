c4d352d29c2c2c42af8fee249e40078c2aea308a
Alex Phillips <exonintrendo@gmail.com>
2015-03-09 19:43:37 -0400

    initial commit

---
+++ src/Titon/Console/Exception/InvalidNumberOfArguments.hh
@@ -0,0 +1,20 @@
+<?hh // strict
+/**
+ * @copyright   2010-2015, The Titon Project
+ * @license     http://opensource.org/licenses/bsd-license.php
+ * @link        http://titon.io
+ */
+
+namespace Titon\Console\Exception;
+
+use RuntimeException;
+
+/**
+ * Exception thrown when parameters are passed in the input that do not belong
+ * to registered input definitions.
+ *
+ * @package Titon\Context\Exception
+ */
+class InvalidNumberOfCommandsException extends RuntimeException {
+
+}

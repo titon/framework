e9229e269b15bc94015e3e8c8a21a8e8be7c02a9
Alex Phillips <exonintrendo@gmail.com>
2015-03-09 17:24:51 -0400

    removed unnecessary EmptyCommandStub test class

--- tests/Titon/Test/Stub/Console/CommandStub.hh
+++ tests/Titon/Test/Stub/Console/CommandStub.hh
@@ -8,7 +8,9 @@ use Titon\Console\InputDefinition\Flag;
 class CommandStub extends AbstractCommand {
 
     public function configure() {
-        $this->addFlag((new Flag('verbose', 'Set command verbosity'))->alias('v'));
+        $this->setName('command');
+        $this->addFlag((new Flag('verbose', 'Set command verbosity'))
+            ->alias('v'));
     }
 
     public function run() {

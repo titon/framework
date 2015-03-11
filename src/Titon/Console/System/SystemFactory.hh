1546bd99ccad38e7b934bf0d1970b1177a73a590
Alex Phillips <exonintrendo@gmail.com>
2015-03-10 20:06:36 -0400

    fixed missing return type

--- src/Titon/Console/System/SystemFactory.hh
+++ src/Titon/Console/System/SystemFactory.hh
@@ -7,6 +7,8 @@
 
 namespace Titon\Console\System;
 
+use Titon\Console\System;
+
 /**
  * Factory class for returning the proper `System` based on the operating system
  * the application is running on.
@@ -18,9 +20,9 @@ class SystemFactory {
     /**
      * Return the proper `System` class based on the system's operating system.
      *
-     * @return \Titon\Console\System\Linux|\Titon\Console\System\Windows
+     * @return \Titon\Console\System
      */
-    public static function factory() {
+    public static function factory(): System {
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
             return new Windows();
         }
